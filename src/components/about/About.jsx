3933b2877dd3ee7b05c85ecd0df130b11c38c58f
Dinesh K <dinesh-9563@csez.zohocorpin.com>
2022-05-12 03:02:10 +0530

    experience

---
+++ src/components/about/About.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import './about.css'
+import ME from '../../assets/me.jpg'
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
+
+const About = () => {
+  return (
+    <section id='about'>
+      <h5>Get to know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>2+ Years of Professional Experience</small>
+            </article>
+
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>300+ Clients</small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>80+completed projects</small>
+            </article>
+          </div>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur ex aliquam, minima ratione voluptatem porro fugit praesentium esse non commodi deleniti perspiciatis quae optio? Obcaecati dolorem reprehenderit neque maxime doloribus?</p>
+
+          <a href="#contact" className='btn btn-primary'>Contact Me</a>
+
+        </div>
+
+      </div>
+
+    </section>
+  )
+}
+
+export default About
